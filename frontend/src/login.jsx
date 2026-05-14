bb27b82d5b3a0b3eabb7d6dd72d5bd90db52c595
Baisakhi Parida <baisakhirparida@gmail.com>
2026-05-14 19:32:16 +0530

    Create login.jsx

---
+++ frontend/src/login.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+
+function Login({ goToSignup, onLogin }) {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const savedUser = JSON.parse(localStorage.getItem("user"));
+
+    if (savedUser && savedUser.email === email && savedUser.password === password) {
+      alert("Login Successful");
+      onLogin();
+    } else {
+      alert("Invalid Credentials");
+    }
+  };
+
+  return (
+    <div className="auth-container">
+      <form className="auth-card" onSubmit={handleSubmit}>
+        <h2>Login</h2>
+
+        <input
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Login</button>
+
+        <p>
+          Don't have an account?
+          <span onClick={goToSignup}> Sign Up</span>
+        </p>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
