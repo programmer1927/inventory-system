0b826e8fa9ba55a476b6f3c6edf1614c0992c5e0
Baisakhi Parida <baisakhirparida@gmail.com>
2026-05-18 09:50:21 +0530

    Update TopSelling.jsx

--- frontend/src/TopSelling.jsx
+++ frontend/src/TopSelling.jsx
@@ -6,7 +6,6 @@ function TopSelling({ products }) {
     .slice(0, 3);
 
   return ( <div style={{ background: "#fff",  borderRadius: "10px" }}>
-      {/* <h2><i>...Top Selling Products...</i></h2> */}
       <p className="card-label">Top Selling Products</p>
       {topSelling.map((item) => ( <div key={item._id}>
           <h4>{item.name}</h4>
